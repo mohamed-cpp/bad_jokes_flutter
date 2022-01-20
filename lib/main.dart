a0d5caff819cb8af1655158254353b2fbbb180a3
Mohamed Gamal <muhamed.gamal9876@gmail.com>
2022-01-20 02:22:46 +0200

    add jokes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return MainApp();
+  }
+}
+
+//class MainApp extends StatelessWidget{ // not refresh screen
+class MainApp extends State<MyApp>{
+
+
+
+  var _jokes = [
+    {"question": "What does a baby computer call its father?", "answer": "Data"}, 
+    {"question": "What's a pencil with two erasers called?", "answer": "Pointless"},
+    {"question": "Why is the calendar always scared?", "answer": "Because it's days are numbered"},
+    {"question": "What do you call a fish with no eye", "answer": "Fsh"},
+  ];
+  var jokesindex = 0 ;
+  nextJoke(int steps){
+      var jokesLength = (_jokes.length - 1);
+      int num = jokesindex + steps;
+    setState(() {
+      jokesindex = num < 0 ? jokesLength : num > jokesLength   ? 0 : num;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,24 +40,24 @@ class MainApp extends StatelessWidget{
           children: <Widget>[
             Container(
               margin: const EdgeInsets.fromLTRB(25, 25, 25, 0),
-              child: const Text("Welcome", 
+              child: Text(_jokes[jokesindex]["question"] as String, 
                     style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold), 
                     textAlign: TextAlign.center),
             ),
             Container(
               margin: const EdgeInsets.all(15),
-              child: const Text("Data", 
+              child: Text(_jokes[jokesindex]["answer"] as String, 
                     style: TextStyle(fontSize: 25), 
                     textAlign: TextAlign.center),
             ),
             Row(mainAxisAlignment: MainAxisAlignment.center,children: [
               Container(
                 margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                child: FloatingActionButton(onPressed: null, child: Icon(Icons.arrow_back, size: 30), backgroundColor: Color.fromRGBO(100, 150, 200, 1),)
+                child: FloatingActionButton(onPressed: (){ nextJoke(-1);}, child: Icon(Icons.arrow_back, size: 30), backgroundColor: Color.fromRGBO(100, 150, 200, 1),)
               ),
               Container(
                 margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                child: FloatingActionButton(onPressed: null, child: Icon(Icons.arrow_forward, size: 30,),backgroundColor: Color.fromRGBO(100, 150, 200, 1)),
+                child: FloatingActionButton(onPressed: (){ nextJoke(1);}, child: Icon(Icons.arrow_forward, size: 30,),backgroundColor: Color.fromRGBO(100, 150, 200, 1)),
               )
             ],)
         ],))
